c14449b63f33a17831e851800db86b6d49eea251
Pavlo Leiko <drawnooblive@gmail.com>
2024-05-20 16:17:46 +0300

    some-custom-quick-styles

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// FORM Code _________________________________________________________________________
-let currentStep = 0;
-const steps = document.querySelectorAll('.step');
-const nextBtn = document.getElementById('nextBtn');
-const prevBtn = document.getElementById('prevBtn');
-const submitBtn = document.getElementById('submitBtn');
-
-function showStep(step) {
-    steps.forEach((stepDiv, index) => {
-        stepDiv.classList.toggle('active', index === step);
-    });
-    prevBtn.style.display = step === 0 ? 'none' : 'inline';
-    nextBtn.style.display = step === steps.length - 1 ? 'none' : 'inline';
-    submitBtn.style.display = step === steps.length - 1 ? 'inline' : 'none';
-}
-
-function changeStep(stepChange) {
-    currentStep += stepChange;
-    showStep(currentStep);
-}
-
 // REDUCE image size _________________________________________________________________
 async function resizeImage(file, maxSizeInKB, maxWidth) {
     return new Promise((resolve, reject) => {
@@ -143,3 +122,6 @@ async function sendEmail() {
         });
 }
 
+
+
+// SIPMLE Form validation ____________________________________________________________
