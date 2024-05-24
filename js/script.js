0fb604a9e6af4d7ed95df412960a15a1f8258c79
Pavlo Leiko <drawnooblive@gmail.com>
2024-05-24 13:36:47 +0300

    more-updates

--- js/script.js
+++ js/script.js
@@ -131,37 +131,6 @@ async function sendEmail() {
         });
 }
 
-
-// SCROLL Animation _____________________________________________________________________
-// function onEntry(entry) {
-//     entry.forEach(change => {
-//       if (change.isIntersecting) {
-//         change.target.classList.add('element-show');
-//       }
-//     });
-//   }
-
-//   let options = { threshold: [0.5] };
-//   let observer = new IntersectionObserver(onEntry, options);
-//   let elements = document.querySelectorAll('.scroll_animation');
-//   for (let elm of elements) {
-//     observer.observe(elm);
-//   }
-
-//   let options1 = { threshold: [0.5] };
-//   let observer1 = new IntersectionObserver(onEntry, options1);
-//   let elements1 = document.querySelectorAll('.scroll_animation_right');
-//   for (let elm of elements1) {
-//     observer1.observe(elm);
-//   }
-
-//   let options2 = { threshold: [0.5] };
-//   let observer2 = new IntersectionObserver(onEntry, options2);
-//   let elements2 = document.querySelectorAll('.scroll_animation_left');
-//   for (let elm of elements2) {
-//     observer2.observe(elm);
-// }
-
 // Accordion _____________________________________________________________________________
 document.addEventListener('DOMContentLoaded', () => {
     const accordion = document.querySelector('.accordion');
